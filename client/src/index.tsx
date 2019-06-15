89d0fc21b2302d37d779b42650fea808174cd557
anton62k <anton62k@gmail.com>
2019-06-15 14:30:01 +0300

    initial version (react, ant design)

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/lib/date-picker/style/css";
+import { Button } from "antd";
+
+ReactDOM.render(<Button>All work!</Button>, document.getElementById("root"));
